867ea7a85e02ca2bcfa567c5455889b4ab227ccc
Sardorbek Pulatov <sardorbek.pulatov@vestiairecollective.com>
2020-05-04 13:30:45 +0200

    fix deferring wg

--- sqs2http.go
+++ sqs2http.go
@@ -103,6 +103,7 @@ func main() {
 	for tmp > 0 {
 		// spawn a goroutine for each worker
 		go func() {
+			defer wg.Done()
 			empties := uint64(0)                                       //<< keeps track of number of subsequent empty replies from queue
 			bo, err := expbo.New(uint64(1000), uint64(*boMax)*1000, 2) //<< exponential backoff
 			if err != nil {
@@ -186,9 +187,6 @@ func main() {
 					fail("sqs delete failure", err)
 				}
 			}
-
-			// if we have broken out of the forever loop then we need to exit gracefully
-			wg.Done()
 		}()
 
 		tmp--
